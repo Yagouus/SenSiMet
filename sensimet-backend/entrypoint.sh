c3372c52f6571a67a23ced35e2e7689f95835357
yagouus <yagofontenla95@gmail.com>
2017-07-03 12:11:42 +0200

    Removed unnecesary files

--- sensimet-backend/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone https://github.com/Yagouus/SenSiMet_Backend /opt/sensimet
-cd /opt/sensimet
-exec gradle bootRun
--- sensimet-frontend/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone https://github.com/Yagouus/SenSiMet_Frontend /tmp/sensimet
-mv -f /tmp/sensimet/* /usr/share/nginx/html
-exec nginx -g 'daemon off;'
